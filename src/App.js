d6031a55c918c3de57917a61a0f17f552a0ed0cf
Beniah <diallobeniah@gmail.com>
2024-03-27 13:32:46 +0000

    Added index and app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Contact from './Contact';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Contact /> 
+      </div>
+    );
+  }
+}
+
+export default App;
