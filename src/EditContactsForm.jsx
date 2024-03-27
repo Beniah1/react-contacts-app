1ab70af896d6c98ca662c1687b0cbac0ffb1f3d4
Beniah <diallobeniah@gmail.com>
2024-03-27 22:07:07 +0000

    ,,

--- src/EditContactsForm.jsx
+++ src/EditContactsForm.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
-class ContactsForm extends Component {
+class EditContactsForm extends Component {
   state = {
-    name: '',
-    phoneNumber: '',
-    location: ''
+    name: this.props.contact.name,
+    phoneNumber: this.props.contact.phoneNumber,
+    location: this.props.contact.location
   };
 
   handleChange = (e) => {
@@ -14,33 +14,47 @@ class ContactsForm extends Component {
   handleSubmit = (e) => {
     e.preventDefault();
     const { name, phoneNumber, location } = this.state;
-    const newContact = { name, phoneNumber, location };
-    this.props.handleAddContact(newContact);
-    this.setState({ name: '', phoneNumber: '', location: '' });
+    const updatedContact = { name, phoneNumber, location };
+    this.props.handleEditContact(updatedContact);
   };
 
   render() {
     return (
       <div>
-        <h2>Add Contact</h2>
+        <h2>Edit Contact</h2>
         <form onSubmit={this.handleSubmit}>
           <div>
             <label>Name: </label>
-            <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
           </div>
           <div>
             <label>Phone Number: </label>
-            <input type="text" name="phoneNumber" value={this.state.phoneNumber} onChange={this.handleChange} />
+            <input
+              type="text"
+              name="phoneNumber"
+              value={this.state.phoneNumber}
+              onChange={this.handleChange}
+            />
           </div>
           <div>
             <label>Location: </label>
-            <input type="text" name="location" value={this.state.location} onChange={this.handleChange} />
+            <input
+              type="text"
+              name="location"
+              value={this.state.location}
+              onChange={this.handleChange}
+            />
           </div>
-          <button type="submit">Add Contact</button>
+          <button type="submit">Update Contact</button>
         </form>
       </div>
     );
   }
 }
 
-export default ContactsForm;
+export default EditContactsForm;
